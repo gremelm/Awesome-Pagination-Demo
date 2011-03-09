aa752d77a078fc2d12a157567f46877385d87ddd
Phil Brown <philip.brown@ima-mt.com>
2011-03-09 23:47:21 +1100

    Completed controller Added standard and AJAX views Added pagination control partial

---
+++ README.md
@@ -0,0 +1,5 @@
+Awesome Pagination Demo
+=======================
+
+This is a sample project demonstrating some awesome pagination using
+the Zend Framework and HTML5 History API.
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,17 +2,36 @@
 
 class IndexController extends Zend_Controller_Action
 {
-
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_helper->ajaxContext->addActionContext('index', 'html')
+                                   ->initContext();
     }
 
     public function indexAction()
     {
-        // action body
+        $paginator = Zend_Paginator::factory($this->getModels());
+        $paginator->setCurrentPageNumber($this->getRequest()->getParam('page', 1));
+        $this->view->paginator = $paginator;
     }
-
-
+    
+    /**
+     * Generate an array of Foo models
+     * 
+     * @return array
+     */
+    private function getModels()
+    {
+        $models = array();
+        for ($i = 0; $i < 100; $i++) {
+            $foo = new Application_Model_Foo(
+                    'foo_' . $i,
+                    'bar_' . $i,
+                    'baz_' . $i
+                    );
+            $models[] = $foo;
+        }
+        return $models;
+    }
 }
 
