184236db5a43ad3a1ece4ceb21d4242536507fc8
Phil Brown <philip.brown@ima-mt.com>
2011-03-10 13:17:44 +1100

    Added animation to AJAX loads

--- public/js/paginator.js
+++ public/js/paginator.js
@@ -2,9 +2,16 @@ jQuery(function($){
     $('.pagination-control').find('a').live('click', function(){
         var link = $(this);
         var container = link.parents('.paged-data-container');
-        $.get(link.attr('href'), { format: 'html' }, function(data){
-            container.html(data);
-        }, 'html');
+        var pos = link.attr('rel') == 'next' ? '-120%' : '120%';
+        container.find('.data').animate({
+            left: pos
+        }, 'slow', function(){
+            var dataContainer = container.find('.paged-data').addClass('loading');
+            $.get(link.attr('href'), { format: 'html' }, function(data){
+                dataContainer.removeClass('loading');
+                container.html(data);
+            }, 'html');
+        });
         return false;
     });
 });
